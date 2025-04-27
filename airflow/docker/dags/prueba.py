900164b7370b7c4e1833feff7ba170a50f198af4
sebas_marv <amarquez@amarquez.com>
2025-04-27 14:52:09 -0500

    Prueba sencilla de pipelines para airflow con git sincronizado

--- airflow/docker/dags/prueba.py
+++ airflow/docker/dags/prueba.py
@@ -5,25 +5,13 @@ from datetime import datetime
 def hello_world():
     print("Hola Mundo")
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'retries': 1,
-}
-
 with DAG(
     dag_id='hola_mundo_dag',
-    default_args=default_args,
-    description='Un DAG que imprime Hola Mundo',
-    schedule= '*/10 * * * *',
-    start_date=datetime(2023, 1, 1),
+    start_date=datetime(2025, 4, 28),
     catchup=False,
-    tags=['example'],
 ) as dag:
 
     task_hello_world = PythonOperator(
         task_id='hello_world_task',
         python_callable=hello_world,
     )
-
-    task_hello_world
